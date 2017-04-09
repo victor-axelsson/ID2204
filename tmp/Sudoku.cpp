3ae7ea607b2aacd8ac7183f5ee5ea6857f00d45e
RubbyRedson <gurnik2009@gmail.com>
2017-04-09 14:33:52 +0200

    ICL distinct stuff

--- tmp/Sudoku.cpp
+++ tmp/Sudoku.cpp
@@ -21,7 +21,7 @@ const int BOARD_INSTANCE = 2;
 //Size of the board, (length of the side)
 const int BOARD_SIZE = 9;
 
-class Sudoku : public Space {
+class Sudoku : public Script {
 protected:
     IntVarArray l;
  
@@ -36,18 +36,106 @@ protected:
     }
     
 public:
-    Sudoku(void) : l(*this, BOARD_SIZE * BOARD_SIZE, 1, 9) {
+	enum {
+		ICL_DEF, ICL_VAL, ICL_BND, ICL_DOM
+	}; 
+	void addDistinct(IntVarArgs array, SizeOptions opt, Sudoku& s) {
+		switch (opt.branching()) {
+		case ICL_DEF:
+			cout << "ICL_DEF" << endl;
+			distinct(s, array, IPL_DEF);
+			break;
+
+		case ICL_VAL:
+			cout << "ICL_VAL" << endl;
+			distinct(s, array, IPL_VAL);
+			break;
+
+		case ICL_BND:
+			cout << "ICL_VAL" << endl;
+			distinct(s, array, IPL_BND);
+			break;
+
+		case ICL_DOM:
+			cout << "ICL_DOM" << endl;
+			distinct(s, array, IPL_DOM);
+			break;
+
+		default:
+			cout << "Default" << endl;
+			//Default
+			distinct(s, array);
+			break;
+		}
+	};
+
+	Sudoku(const SizeOptions& opt)
+		: Script(opt), l(*this, BOARD_SIZE * BOARD_SIZE, 1, 9) {
 
         printLabel();
         
         //rows
         for (int i = 0; i < BOARD_SIZE; i++) {
-            distinct(*this, l.slice(i * BOARD_SIZE, 1, BOARD_SIZE));
+			//addDistinct(l.slice(i * BOARD_SIZE, 1, BOARD_SIZE), opt, *this);
+			switch (opt.branching()) {
+			case ICL_DEF:
+				cout << "ICL_DEF" << endl;
+				distinct(*this, l.slice(i * BOARD_SIZE, 1, BOARD_SIZE), IPL_DEF);
+				break;
+
+			case ICL_VAL:
+				cout << "ICL_VAL" << endl;
+				distinct(*this, l.slice(i * BOARD_SIZE, 1, BOARD_SIZE), IPL_VAL);
+				break;
+
+			case ICL_BND:
+				cout << "ICL_VAL" << endl;
+				distinct(*this, l.slice(i * BOARD_SIZE, 1, BOARD_SIZE), IPL_BND);
+				break;
+
+			case ICL_DOM:
+				cout << "ICL_DOM" << endl;
+				distinct(*this, l.slice(i * BOARD_SIZE, 1, BOARD_SIZE), IPL_DOM);
+				break;
+
+			default:
+				cout << "Default" << endl;
+				//Default
+				distinct(*this, l.slice(i * BOARD_SIZE, 1, BOARD_SIZE));
+				break;
+			}
         }
          
         //columns
         for (int i = 0; i < BOARD_SIZE; i++) {
-            distinct(*this, l.slice(i, BOARD_SIZE, BOARD_SIZE));
+			//addDistinct(l.slice(i, BOARD_SIZE, BOARD_SIZE), opt, *this);
+			switch (opt.branching()) {
+			case ICL_DEF:
+				cout << "ICL_DEF" << endl;
+				distinct(*this, l.slice(i, BOARD_SIZE, BOARD_SIZE), IPL_DEF);
+				break;
+
+			case ICL_VAL:
+				cout << "ICL_VAL" << endl;
+				distinct(*this, l.slice(i, BOARD_SIZE, BOARD_SIZE), IPL_VAL);
+				break;
+
+			case ICL_BND:
+				cout << "ICL_VAL" << endl;
+				distinct(*this, l.slice(i, BOARD_SIZE, BOARD_SIZE), IPL_BND);
+				break;
+
+			case ICL_DOM:
+				cout << "ICL_DOM" << endl;
+				distinct(*this, l.slice(i, BOARD_SIZE, BOARD_SIZE), IPL_DOM);
+				break;
+
+			default:
+				cout << "Default" << endl;
+				//Default
+				distinct(*this, l.slice(i, BOARD_SIZE, BOARD_SIZE));
+				break;
+			}
         }
         
         //squares
@@ -57,7 +145,32 @@ public:
                 for (int rowNum = 0; rowNum < sqrt(BOARD_SIZE); rowNum++) {
                     square << l.slice(rowNum * BOARD_SIZE + i + j, 1, sqrt(BOARD_SIZE));
                 }
-                distinct(*this, square);
+				//addDistinct(square, opt);
+				switch (opt.branching()) {
+				case ICL_DEF:
+					distinct(*this, square, IPL_DEF);
+					break;
+
+				case ICL_VAL:
+					distinct(*this, square, IPL_VAL);
+					break;
+
+				case ICL_BND:
+					cout << "ICL_VAL" << endl;
+					distinct(*this, square, IPL_BND);
+					break;
+
+				case ICL_DOM:
+					cout << "ICL_DOM" << endl;
+					distinct(*this, square, IPL_DOM);
+					break;
+
+				default:
+					cout << "Default" << endl;
+					//Default
+					distinct(*this, square);
+					break;
+				}
             }
         }
    
@@ -78,8 +191,10 @@ public:
         
         branch(*this, l, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
     }
+
+	
     
-    Sudoku(bool share, Sudoku& s) : Space(share, s) {
+    Sudoku(bool share, Sudoku& s) : Script(share, s) {
         l.update(*this, share, s.l);
     }
     
@@ -87,31 +202,39 @@ public:
         return new Sudoku(share, *this);
     }
 
-    void print(void) const {
+	virtual void
+		print(std::ostream& os) const {
         for(int i = 0; i < BOARD_SIZE * BOARD_SIZE; i+= BOARD_SIZE){
             if(i % 27 == 0){
-                cout << "-------------------------" <<endl;
+				os << "-------------------------" <<endl;
             }
             
-            cout << "|" << l[i + 0] << ", " << l[i + 1] << ", " << l[i + 2] << "|" << l[i + 3] << ", " << l[i + 4] << ", " << l[i + 5] << "|" << l[i + 6]<< ", "  << l[i + 7] << ", " << l[i + 8] << "|" <<endl;
+			os << "|" << l[i + 0] << ", " << l[i + 1] << ", " << l[i + 2] << "|" << l[i + 3] << ", " << l[i + 4] << ", " << l[i + 5] << "|" << l[i + 6]<< ", "  << l[i + 7] << ", " << l[i + 8] << "|" <<endl;
         }
         
-        cout << "-------------------------" <<endl;
+		os << "-------------------------" <<endl;
         
         
-        cout <<endl <<endl;
+		os <<endl <<endl;
     }
 };
 
 int main(int argc, char* argv[]) {
-    Sudoku* m = new Sudoku;
-    Gecode::DFS<Sudoku> e(m);
-    delete m;
-    
-    while (Sudoku* s = e.next()) {
-        s->print();
-        delete s;
-    }
-    
+	SizeOptions opt("Sudoku");
+    Sudoku* m = new Sudoku(opt);
+
+	//cout << "ICL_DEF" << endl;
+	//opt.branching(Sudoku::ICL_DEF);
+
+	cout << "ICL_VAL" << endl;
+    opt.branching(Sudoku::ICL_VAL);
+
+	//cout << "ICL_BND" << endl;
+    //opt.branching(Sudoku::ICL_BND);
+
+	//cout << "ICL_DOM" << endl;
+    //opt.branching(Sudoku::ICL_DOM);
+
+	Script::run<Sudoku, DFS, SizeOptions>(opt);
     return 0;
 }
