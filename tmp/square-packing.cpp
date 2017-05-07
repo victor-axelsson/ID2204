da0c61bfda927fde4f23bcd7e0599acde003215b
Victor Axelsson <victor_axelsson@hotmail.com>
2017-05-07 14:39:15 +0200

    Implemented a naive implementation of the packing problem

--- tmp/square-packing.cpp
+++ tmp/square-packing.cpp
@@ -13,7 +13,7 @@
 #include <gecode/driver.hh>
 #include <math.h>
 #include <gecode/minimodel.hh>
-
+#include "Examples.cpp"
 
 using namespace Gecode;
 using namespace std;
@@ -23,6 +23,8 @@ class SquarePacking :  public Script {
     
 private:
     static int size(int i, int n){
+        //[0, 1, 2, 3, 4, 5] == index
+        //[6, 5, 4, 3, 2, 1] == size from n
         return n - i;
     }
     
@@ -30,16 +32,17 @@ protected:
     IntVarArray x;
     IntVarArray y;
     IntVar s;
+    int n;
     
 public:
     
-    
-    SquarePacking(const SizeOptions& opt): Script(opt),
-    x(*this, opt.size(), 0, opt.size()),
-    y(*this, opt.size(), 0, opt.size()),
-    s(*this, opt.size() + opt.size() -1, opt.size() + opt.size() -1){
+    SquarePacking(const SizeOptions& opt): Script(opt){
+        n = opt.size();
+
+        x = IntVarArray(*this, opt.size() , 0, opt.size());
+        y = IntVarArray(*this, opt.size() , 0, opt.size());
+        s = IntVar(*this, 0, opt.size() + opt.size() -1);
         
-        int n = opt.size();
         
         //Make sure the x and y are within the boundries of s
         for(int i = 0; i < n; i++){
@@ -73,8 +76,8 @@ public:
                 rel(*this, left + right + below + above > 0);
                 
                 cout <<"I: " << i << ", Z: " << z <<endl;
-                cout << "x[z]" << x[z] << " x[i]" << x[i] << " y[i]" << y[i] << " y[z]" << y[z] <<endl;
-                cout << "Left:" << left << " Right:" << right << " Below:" << below << " Above:" << above << endl << endl;
+                //cout << "x[z]" << x[z] << " x[i]" << x[i] << " y[i]" << y[i] << " y[z]" << y[z] <<endl;
+                //cout << "Left:" << left << " Right:" << right << " Below:" << below << " Above:" << above << endl << endl;
                 
             }
         }
@@ -98,6 +101,7 @@ public:
         x.update(*this, share, tmp.x);
         y.update(*this, share, tmp.y);
         s.update(*this, share, tmp.s);
+        n = tmp.n;
     }
     
     virtual Space* copy(bool share) {
@@ -108,19 +112,28 @@ public:
     //Print solution
     virtual void
     print(std::ostream& os) const {
-        /*
-         for (int i = 0; i < l.size(); i++) {
-         os << l[i] << ", ";
-         if ((i + 1) % 10 == 0)
-         os << std::endl << "\t";
-         }
-         os << std::endl;
-         */
+        
+        cout << "convert -size " << 11 << "x" << 11 << " xc:none ";
+        
+        for(int i = 0; i < n; i++){
+            
+            int x1 = x[i].min();
+            int y1 = y[i].min();
+            int x2 = x[i].min() + size(i, n) -1;
+            int y2 = y[i].min() + size(i, n) -1;
+            
+            if(size(i, n) == 1){
+                cout << "  -fill "<< colors[i % 7] <<" -draw \" color " << x1 << "," << y1 << " point " << "\" ";
+            }else{
+                cout << "  -fill "<< colors[i % 7] <<" -draw \" rectangle " << x1 << "," << y1 <<" " << x2 <<"," << y2  << "\" ";
+            }
+        }
+        
+        cout << " -scale 600% squares.jpg ";
+        
         os << "X: " << x <<endl << "Y:" << y <<endl <<"S: " << s;
     }
 };
-
-
 
 
 int main(int argc, char* argv[]) {
